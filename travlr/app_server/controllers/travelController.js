ec821dbe635edbf6e471a0b6f7cb053a493c5d10
Francis Cottrell-Eshaghi <cottrell-eshaghi@protonmail.com>
2024-06-02 18:50:33 -0400

    m3fixesnotest

--- travlr/app_server/controllers/travelController.js
+++ travlr/app_server/controllers/travelController.js
@@ -1,3 +1,5 @@
+// modules and defined travelDestinations array
+
 exports.travel = (req, res) => {
-  res.render('travel', { title: 'Travel' });
+  res.render('travel', { title: 'Travel', destinations: travelDestinations });
 };
--- travlr/app_server/routes/travel.js
+++ travlr/app_server/routes/travel.js
@@ -1,7 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var travelController = require('../controllers/travelController');
 
-router.get('/', travelController.travel);
+//  data to pass to the view
+var travelDestinations = [
+  {
+    name: 'Gale Reef',
+    image: 'images/reef1.jpg',
+    description: [
+      'Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis.',
+      'Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet.'
+    ]
+  },
+  {
+    name: 'Dawson’s Reef',
+    image: 'images/reef2.jpg',
+    description: [
+      'Integer magna leo, posuere et dignissim vitae, porttitor at odio. Pellentesque a metus nec magna placerat volutpat. Nunc nisi mi, elementum sit amet aliquet quis, tristique quis nisl. Curabitur odio lacus, blandit ut hendrerit.',
+      'Vulputate, vulputate at est. Morbi aliquet viverra metus eu consectetur. In lorem dui, elementum sit amet convallis ac, tincidunt vel sapien.'
+    ]
+  },
+  {
+    name: 'Claire’s REEF',
+    image: 'images/reef3.jpg',
+    description: [
+      'Donec sed felis risus. Nulla facilisi. Donec a orci tellus, et auctor odio. Fusce ac orci nibh, quis semper arcu. Cras orci neque, euismod et accumsan ac, sagittis molestie lorem. Proin odio sapien, elementum at tempor non.',
+      'Vulputate eget libero. In hac habitasse platea dictumst. Integer purus justo, egestas eu consectetur eu, cursus in tortor. Quisque nec nunc ac mi ultrices iaculis.'
+    ]
+  },
+  {
+    name: 'New Place',
+    image: 'images/new_place.jpg',
+    description: [
+      'Description of the new place added.'
+    ]
+  }
+];
+
+// GET travel page
+router.get('/', function(req, res, next) {
+  res.render('travel', { destinations: travelDestinations });
+});
 
 module.exports = router;
